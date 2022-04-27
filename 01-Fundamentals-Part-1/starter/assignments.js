20d3247ec4670fcef3d3b106e7cfed7752c80e50
Chris Parker <chris@parkerdesignbuild.com>
2022-04-26 20:52:35 -0400

    section 2 work

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+// values and variables
+let country = "USA";
+let continent = "North America";
+let population = 350_000_000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// data types
+let isIsland = false;
+let language;
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("Chris");
+console.log("38");
+
+let firstName = "Chris";
+console.log(firstName);
+
+let myFirstJob = "Contractor";
+let myNextJob = "Programmer";
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+
+javascriptIsFun = "YES";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
