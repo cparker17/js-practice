191e8a36fb5d7178cf288031cee8225d3423db91
Chris Parker <chris@parkerdesignbuild.com>
2022-05-07 06:42:33 -0400

    started section 8

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, You are ${age}, born in ${birthYear}.`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}.`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Chris';
+calcAge(1984);
