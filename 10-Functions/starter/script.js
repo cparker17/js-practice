1efad400426a281851391b3d5e22bc0884c8754c
Chris Parker <chris@parkerdesignbuild.com>
2022-05-11 20:00:37 -0400

    section 10 completed

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -91,3 +91,25 @@ const eurowings = {
   iataCode: 'EW',
   bookings: [],
 };
+
+(function () {
+  console.log('This will never run again');
+})();
+
+// closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
